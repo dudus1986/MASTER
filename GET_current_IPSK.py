89beb0a84b16786d38eec8efb6744a66658e4c32
dudas1986 <jameskin@cisco.com>
2020-07-03 14:40:48 +0100

    Update GET_current_IPSK.py

--- GET_current_IPSK.py
+++ GET_current_IPSK.py
@@ -1,16 +1,9 @@
-"""create 3 wifi users"""
-
 """ requests is the module that is for HTTPS requests using python"""
 import requests
 
-""" my org id is id is 454861 - network ID is L_659777345409787443 for Crewe """
+"""   my org id is id is 454861 - network ID is L_659777345409787443 for Crewe """
 
 """need to use V1 for IPSK"""
-
-
-
-
-
 url = "https://api.meraki.com/api/v1/networks/L_659777345409787443/wireless/ssids/5/identityPsks"
 
 headers = {"X-Cisco-Meraki-API-Key": "3a18562c3c7888509be73b4ff6e46f53b24dbfa5"}
